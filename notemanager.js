a9bc051e91cc0e611a4330c1a752164e67510771
Vinayak Agarwal <4vagarwal@gmail.com>
2020-05-27 16:13:01 +0530

    notetaker update3.4

--- notemanager.js
+++ notemanager.js
@@ -36,10 +36,10 @@ var q = 1;
 function darkTheme() {
   if (q % 2 == 1) {
     document.body.style.backgroundColor = '#2b2b28';
-    document.getElementById('darkTheme').innerHTML = 'LightMode';
+    document.getElementById('darkTheme').innerHTML = 'Light theme';
   } else {
     document.body.style.background = '#ebebe3';
-    document.getElementById('darkTheme').innerHTML = 'DarkMode';
+    document.getElementById('darkTheme').innerHTML = 'Dark theme';
   }
   q++;
 }
