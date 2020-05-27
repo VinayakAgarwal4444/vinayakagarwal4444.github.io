05af876babf18dba534810ed1d08bdeb4aa890b7
Vinayak Agarwal <4vagarwal@gmail.com>
2020-05-27 14:18:36 +0530

    notemaker update1

--- notemanager.js
+++ notemanager.js
@@ -1,7 +1,9 @@
+//function to add note
+
 function addNote() {
   var note = document.createElement('div');
   note.setAttribute('class', 'note');
-
+  note.style.transform = `rotate(${randomInteger(-5, 5)}deg)`;
   note.innerHTML = `
     
 
@@ -21,4 +23,23 @@ function addNote() {
 
 function deleteNote(oppa) {
   oppa.remove();
+}
+
+//random no. generator
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var q = 1;
+
+function darkTheme() {
+  if (q % 2 == 1) {
+    document.body.style.backgroundColor = '#2b2b28';
+    document.getElementById('darkTheme').innerHTML = 'LightMode';
+  } else {
+    document.body.style.background = '#ebebe3';
+    document.getElementById('darkTheme').innerHTML = 'DarkMode';
+  }
+  q++;
 }
